5ad02d86717a7a0ecb541acfb8780f5563fcc005
ThiesMS <thies.schulze@gmail.com>
2025-01-29 16:33:42 +0100

    improved comments

--- fcsr_agilent_1_7.py
+++ fcsr_agilent_1_7.py
@@ -6,12 +6,12 @@ ch1.uv file
 """
 
 
-import re # Für Regex-Suche
-import os # für allgemeine dateimanipulationen
-import shutil #für ordner-kopieren
-import configparser # configfile für start, ziel und subfoldernamen
-import logging #für Logging
-import sys
+import re # for Regex-Search
+import os # for general data manipulation
+import shutil #for folder copying
+import configparser # configfile for source, target and sub-folder names
+import logging #for logging
+import sys #for graceful exiting
 
 
 def findSubfolderName(regexInitials, sampleNameString, folderNoInitials, initialsList):
@@ -24,26 +24,26 @@ def findSubfolderName(regexInitials, sampleNameString, folderNoInitials, initial
     logging.info('Found initials: ' + regexInitials)
     
     #Compare initials to initialsList and return matches
-    regexInitialsList = re.compile(regexInitials, re.IGNORECASE) #damit sample names xy, XY und Xy alle in den Ordner Xy gehen
-    subfolderList = [m.group(0) for initial in initialsList for m in [regexInitialsList.search(initial)] if m] #gibt ergebnisliste mit einem Element aus
-    subfolder = ''.join(subfolderList) #konvertiert Liste in String // Might be problematic, if the list contains the match multiple times
+    regexInitialsList = re.compile(regexInitials, re.IGNORECASE) #so sample names xy, XY und Xy all go to target Xy
+    subfolderList = [m.group(0) for initial in initialsList for m in [regexInitialsList.search(initial)] if m] #result list with one element
+    subfolder = ''.join(subfolderList) #convert list to String // Might be problematic, if the list contains the match multiple times
     if subfolder == '':
         return folderNoInitials
     return subfolder
 
 def copyFolderToDestination(parentFolder, destinationPath, subfolder, sampleNameString, instrumentSuffix):
-    """Ordner in den entsprechenden subfolder verschieben, gucken, dass nichts überschrieben wird"""
+    """Move folders to respective subfolders, make sure nothing is overwritten"""
     try:
         shutil.copytree(parentFolder, os.path.join(destinationPath, subfolder, (sampleNameString + ' ' + instrumentSuffix + '.d')))
         print('Saved in subfolder: ' + subfolder + ' as: ' + sampleNameString + ' ' + instrumentSuffix + '.d')
         logging.info('Saved in subfolder: ' + subfolder + ' as: ' + sampleNameString + ' ' + instrumentSuffix + '.d')
-    except FileExistsError: #wenn es den Ordner schon gibt...
+    except FileExistsError: #if folder already exists...
         print('Folder ' + sampleNameString + ' ' + instrumentSuffix + '.d already exists, retrying.')
         logging.info('Folder ' + sampleNameString + ' ' + instrumentSuffix + '.d already exists, retrying.')
         copynumber = 0
         while True:
             copynumber = copynumber + 1
-            try: #... dann wird an den Ordnernamen erst xyz-1.d, dann xyz-2.d etc. angehängt, bis ein neuer Ordnername gefunden wurde
+            try: #... folder name gets appended with xyz-1.d, xyz-2.d etc. until a new folder name is found
                 shutil.copytree(parentFolder, os.path.join(destinationPath, subfolder, (sampleNameString + '-' + str(copynumber) + ' ' + instrumentSuffix + '.d')))
                 print('Saved in subfolder: ' + subfolder + ' as: ' + sampleNameString + '-' + str(copynumber) + ' ' + instrumentSuffix + '.d')
                 logging.info('Saved in subfolder: ' + subfolder + ' as: ' + sampleNameString + '-' + str(copynumber) + ' ' + instrumentSuffix + '.d')
@@ -54,7 +54,7 @@ def copyFolderToDestination(parentFolder, destinationPath, subfolder, sampleName
                 continue
 
 def walklevel(some_dir, level=1):
-    """Wie os.walk, aber wird nach einer bestimmten Suchtiefe abgebrochen"""
+    """like os.walk, but stops at a certain depth"""
     some_dir = some_dir.rstrip(os.path.sep)
     try:
         assert os.path.isdir(some_dir)
@@ -69,41 +69,41 @@ def walklevel(some_dir, level=1):
         if num_sep + level <= num_sep_this:
             del dirs[:]
 
-if __name__ == "__main__":
+if __name__ == "__main__":   #initialize main loop
 
     #Setup logging
     logging.basicConfig(format='%(asctime)s %(message)s',filename=r'fcsr_agilent.log',level=logging.DEBUG)
     logging.info('Program started.')
 
-    #Ini-File Öffnen
+    #open Ini file
     configparser = configparser.RawConfigParser()
-    configFilePath = r'fcsr_agilent.ini' #die .ini soll im selben Ordner liegen wie fcsr_agilent.py
+    configFilePath = r'fcsr_agilent.ini' #.ini should be in the same path as fcsr_agilent.py
     configparser.read(configFilePath)
-    #Pfade aus datei einlesen
+    #get paths from file
     sourcePath = configparser.get('paths', 'sourcePath')
     destinationPath = configparser.get('paths', 'destinationPath')
-    #Liste der Initialen aus datei einlesen
+    #get list of initials from file
     initialsList = [e.strip() for e in configparser.get('initials', 'initialsList').split(',')]
-    #Zielordner für Files einlesen, die sich nicht zuordnen lassen
+    #get name for fodler for all runs that have unmatched initials
     folderNoInitials = configparser.get('initials', 'folderNoInitials')
-    #Suchtiefe einlesen
+    #get search depth
     searchDepth = int(configparser.get('parameters', 'searchDepth'))
-    #Bezeichnung des Geräts für Filename einlesen (xy 1234 ESI, xy 1234 GCMS, ...)
+    #get instrument suffix for filename (xy 1234 ESI, xy 1234 GCMS, ...)
     instrumentSuffix = configparser.get('parameters', 'instrumentSuffix')
 
-    #Zielordner anlegen, wenn noch nicht vorhanden
+    #make target folders if not already existing
     for initials in initialsList:
         os.makedirs(os.path.join(destinationPath, initials), exist_ok = True)
 
-    #Liste der bereits verarbeiteten Läufe einlesen
+    #get list of already processed folders, so the script will not try to copy the same folder multiple times when executed repeatedly
     try:
         with open(r'processed_folders.txt', 'r') as procFolders:
             procFoldersList = procFolders.read().splitlines()
-    except FileNotFoundError:
+    except FileNotFoundError: #on first run, or if processed_folders.txt was deleted, recreate
         print('processed_folders.txt does not exist, creating it...')
         logging.info('processed_folders.txt does not exist, creating it...')
         with open(r'processed_folders.txt', "w") as file:
-            pass  # Just create an empty file
+            pass  # Create processed_folders.txt as empty file
         with open(r'processed_folders.txt', 'r') as procFolders:
             procFoldersList = procFolders.read().splitlines()
 
@@ -132,7 +132,7 @@ if __name__ == "__main__":
                     else:
                         print('Found folder: ' + (tops) + '. Trying to open file: ' + (file))
                         logging.info('Found folder: ' + (tops) + '. Trying to open file: ' + (file))
-                        #Sample name string in *.uv finden
+                        #find Sample name string in *.uv
                         sourceFileLocation = os.path.join(tops, file)
                         with open(sourceFileLocation, encoding ="cp1252", errors ='ignore') as sourceFile:
                             sourceFile.read(858) #discards the first 858 bytes // maybe use seek?
@@ -158,9 +158,9 @@ if __name__ == "__main__":
                             logging.info('Generated new folder name: ' + sampleNameString)
 
                         #Find Initials in sampleNameString
-                        regexInitials = '' #regexInitials hat zunächst keine Treffer
-                        for groups in sampleNameRegex.findall(sampleNameString): #jetzt wird gesucht
-                            regexInitials = groups[1] #Die Initialen gehen in den String regexInitials // may need to use index 0 without the outer capture group
+                        regexInitials = '' #regexInitials initially has no matches
+                        for groups in sampleNameRegex.findall(sampleNameString): #searching...
+                            regexInitials = groups[1] #found initials String regexInitials // may need to use index 0 without the outer capture group
                             break
 
                         #inserts a space between Initials and Rest of Sample Name by first removing the Initials, then stripping preceding whitespace, then re-entering initials and a Space
@@ -171,12 +171,12 @@ if __name__ == "__main__":
                         logging.info('Run will be saved in subfolder: ' + subfolder)
 
                         copyFolderToDestination(tops, destinationPath, subfolder, sampleNameString, instrumentSuffix)
-                        #verarbeiteten Ordner an die 'processed_folders.txt' anhängen
+                        #add folder to 'processed_folders.txt' so it will not be processed multiple times on repeat execution of the script
                         with open(r'processed_folders.txt', 'a') as procFolders:
                             procFolders.write(tops + '\n')
                     print('')
                     logging.info('')
 
-    #Schleife beendet, nächsten Folder finden
+    #Loop finished, done.
     print('Program finished.')
     logging.info('Program finished.')
